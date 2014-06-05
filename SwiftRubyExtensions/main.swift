c2f08d2414c31500f058279366948522208e40b3
Rakesh <rakesh@Rakeshs-Mac-Pro.local>
2014-06-05 19:51:23 +0530

    Initial Commit

---
+++ SwiftRubyExtensions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftRubyExtensions
+//
+//  Created by Rakesh on 05/06/14.
+//  Copyright (c) 2014 Rakesh. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
